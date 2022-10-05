9d2156a1aa8a7de9325b74dac472deb5ddade61e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 23:32:44 +0000

    Adding tarfile member sanitization to extractall()

--- lib/climateData.py
+++ lib/climateData.py
@@ -93,7 +93,26 @@ class WeatherData(object):
         try:
             # Extract the tar file
             with tarfile.open(fname, 'r') as tar:
-                tar.extractall(tmpPath)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, tmpPath)
             
             # Read the stations
             for fname in os.listdir(tmpPath + "/ghcnd_gsn"):
